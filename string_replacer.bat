27a4fae1e87b67ce77b3ce7c37c8c4e5e543b124
12q1 <thomasham89@gmail.com>
2020-10-22 18:27:00 +0200

    initial commit

---
+++ string_replacer.bat
@@ -0,0 +1,28 @@
+::Put this file in the directory you want it to run in
+@echo off
+echo Starting string replacer...
+
+setlocal EnableExtensions DisableDelayedExpansion
+
+set "search=somestring"
+:: change 'somestring' with your original path
+set "replace=anotherstring"
+:: change 'anotherstring' to your desired path
+
+set "targetFile=*.mod"
+:: searches for all files with the extension '.mod'
+set "rootDir=."
+
+for %%j in ("%rootDir%\%targetFile%") do (
+    for /f "delims=" %%i in ('type "%%~j" ^& break ^> "%%~j"') do (
+        set "line=%%i"
+        setlocal EnableDelayedExpansion
+        set "line=!line:%search%=%replace%!"
+        >>"%%~j" echo(!line!
+        endlocal
+    )
+)
+
+echo Finished replacing strings
+
+endlocal
